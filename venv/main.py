dd572eeba0ec3ab2a62296f812dc70223d35d352
gokultej <38723397+gokultej@users.noreply.github.com>
2023-08-06 17:02:57 +0530

    Send an email with news titles and descriptions

--- venv/main.py
+++ venv/main.py
@@ -1,14 +1,19 @@
 import requests
-
+from send_email import send_email
 api_key="e77c8b88d9f14977b733519602dcacd9"
 
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-07-06&sortBy=publishedAt&apiKey=e77c8b88d9f14977b733519602dcacd9"
 
+#Make Request
 request =requests.get(url)
-content = request.json()
+
 #get a dictionary with data
-print(content["articles"])
+content = request.json()
 
 #Access the article titles and description
+body =""
 for article in content["articles"]:
-    print(artilce["title"])
+    if article["title"] is not None:
+        body = body + article["title"] +"\n"+ article["description"] + 2*"\n"
+body = body.encode("utf-8")
+send_email(body)
