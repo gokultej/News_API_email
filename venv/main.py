359a3ef4ae309fb4d2482e4bc4a9a95dbbd258a2
gokultej <38723397+gokultej@users.noreply.github.com>
2023-08-06 17:13:29 +0530

    Add links,specify language,limit news,add subject,control category

--- venv/main.py
+++ venv/main.py
@@ -1,8 +1,15 @@
 import requests
 from send_email import send_email
-api_key="e77c8b88d9f14977b733519602dcacd9"
 
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-07-06&sortBy=publishedAt&apiKey=e77c8b88d9f14977b733519602dcacd9"
+
+topic ="tesla"
+
+api_key="e77c8b88d9f14977b733519602dcacd9"
+url = "https://newsapi.org/v2/everything?" \
+      "q=tesla&" \
+      "from=2023-07-06&" \
+      "sortBy=publishedAt&" \
+      "apiKey=e77c8b88d9f14977b733519602dcacd9"
 
 #Make Request
 request =requests.get(url)
@@ -12,8 +19,10 @@ content = request.json()
 
 #Access the article titles and description
 body =""
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
-        body = body + article["title"] +"\n"+ article["description"] + 2*"\n"
+        body = "Subject:Today's NEWS"+"\n"+ body + article["title"] +"\n"+\
+               article["description"]\
+               + "\n" +ariticle["url"] +2*"\n"
 body = body.encode("utf-8")
 send_email(body)
